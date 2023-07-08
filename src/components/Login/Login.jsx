03511ee296a63e806da544211dfb19b621725d63
CrhistianT <andrewturpo573@gmail.com>
2023-07-08 10:24:28 -0500

    feat: login and signup connected to services and working

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,22 +1,35 @@
 import { useState } from "react"
 
 import { CredentialsForm } from "../../ui/CredentialsForm/CredentialsForm"
+import { useAuth } from "../../context/authContext"
 import Input from "../../ui/Input/Input"
 import Button from "../../ui/Button/Button"
 
-
 const Login = () => {
+  const {login} = useAuth()
 
   const [formData, setFormData] = useState({
-    email: null,
-    password: null,
+    email: 'crhis@mail.com',
+    password: 'letmein',
   })
 
+  const { email, password } = formData;
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    login(formData);
+  }
+
   return (
-    <CredentialsForm>
+    <CredentialsForm onSubmit={handleSubmit}>
       <div>
-        <Input type="email" label="Email address"/>
-        <Input type="password" label="Password" />
+        <Input type="email" name="email" label="Email address" value={email} onChange={handleChange} />
+        <Input type="password" name="password" label="Password" value={password} onChange={handleChange} />
       </div>
       <Button type="primary" size="lg" isFullWidth rounded>Log in</Button>
     </CredentialsForm>)
